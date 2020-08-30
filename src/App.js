95e9406aa53baa1b09c1bc5a5085d7e7932560d9
mkleung-dev <mkleung@example.com>
2020-08-30 15:05:28 +0800

    Notes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
 import './App.css';
+import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
+import { AppContext } from "./libs/contextLib";
+import { Auth } from "aws-amplify";
+import { onError } from "./libs/errorLib";
 
 function App() {
+  const history = useHistory();
+  const [isAuthenticating, setIsAuthentication] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+  
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        onError(e);
+      }
+    }
+    setIsAuthentication(false);
+  }
+
+  async function handleLogout() {
+    await Auth.signOut();
+
+    userHasAuthenticated(false);
+    history.push("/login");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    !isAuthenticating &&
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Note Taking</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            {isAuthenticated
+              ? <NavItem onClick={handleLogout}>Logout</NavItem>
+              : <>
+                  <LinkContainer to="/signup">
+                    <NavItem>Signup</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>Login</NavItem>
+                  </LinkContainer>
+                </>
+            }
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <AppContext.Provider value={{isAuthenticated, userHasAuthenticated}}>
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    s3: {
+        REGION: "TODO",
+        BUCKET: "TODO"
+    },
+    apiGateway: {
+        REGION: "TODO",
+        URL: "TODO"
+    },
+    cognito: {
+        REGION: "TODO",
+        USER_POOL_ID: "TODO",
+        APP_CLIENT_ID: "TODO",
+        IDENTITY_POOL_ID: "TODO"
+    }
+};
